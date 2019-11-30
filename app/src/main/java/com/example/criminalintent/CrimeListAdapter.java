3f75da30c0358e0a20f2ab9370c22d521432d3fb
Javernaut <javernaut@gmail.com>
2019-11-30 12:37:27 +0200

    Moving CrimeViewHolder into a separate file

--- app/src/main/java/com/example/criminalintent/CrimeListAdapter.java
+++ app/src/main/java/com/example/criminalintent/CrimeListAdapter.java
@@ -1,10 +1,12 @@
 package com.example.criminalintent;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,6 +24,7 @@ public class CrimeListAdapter extends RecyclerView.Adapter<CrimeViewHolder> {
     @NonNull
     @Override
     public CrimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.e("CrimeListAdapter", "onCreateViewHolder");
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View itemView = inflater.inflate(R.layout.item_crime, parent, false);
 
@@ -30,6 +33,7 @@ public class CrimeListAdapter extends RecyclerView.Adapter<CrimeViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull CrimeViewHolder holder, int position) {
+        Log.e("CrimeListAdapter", "onBindViewHolder - position " + position);
         Crime crime = crimes.get(position);
         holder.bindTo(crime);
     }
@@ -37,30 +41,5 @@ public class CrimeListAdapter extends RecyclerView.Adapter<CrimeViewHolder> {
     @Override
     public int getItemCount() {
         return crimes.size();
-    }
-}
-
-class CrimeViewHolder extends RecyclerView.ViewHolder {
-
-    private Crime crime;
-
-    private TextView titleView;
-    private TextView dateView;
-    private CheckBox solvedCheckBox;
-
-    public CrimeViewHolder(@NonNull View itemView) {
-        super(itemView);
-
-        titleView = itemView.findViewById(R.id.title);
-        dateView = itemView.findViewById(R.id.date);
-        solvedCheckBox = itemView.findViewById(R.id.solved);
-    }
-
-    public void bindTo(Crime crime) {
-        this.crime = crime;
-
-        titleView.setText(crime.getTitle());
-        dateView.setText(crime.getDate().toString());
-        solvedCheckBox.setChecked(crime.isSolved());
     }
 }
---
+++ app/src/main/java/com/example/criminalintent/CrimeViewHolder.java
@@ -0,0 +1,46 @@
+package com.example.criminalintent;
+
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class CrimeViewHolder extends RecyclerView.ViewHolder {
+
+    private Crime crime;
+
+    private TextView titleView;
+    private TextView dateView;
+    private CheckBox solvedCheckBox;
+
+    private final View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(
+                    v.getContext(),
+                    crime.getTitle() + " was clicked",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public CrimeViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        titleView = itemView.findViewById(R.id.title);
+        dateView = itemView.findViewById(R.id.date);
+        solvedCheckBox = itemView.findViewById(R.id.solved);
+
+        itemView.setOnClickListener(clickListener);
+    }
+
+    public void bindTo(Crime crime) {
+        this.crime = crime;
+
+        titleView.setText(crime.getTitle());
+        dateView.setText(crime.getDate().toString());
+        solvedCheckBox.setChecked(crime.isSolved());
+    }
+}
