0e40228622c59c1e00ecf744f319c20efd44d7e0
Javernaut <javernaut@gmail.com>
2019-11-30 11:00:00 +0200

    Dummy data, basic layout and RecyclerView dependency

--- app/src/main/java/com/example/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeListActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CrimeListActivity extends AppCompatActivity {
+
+    private List<Crime> crimes = generateDemoCrimes();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +17,14 @@ public class CrimeListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private static List<Crime> generateDemoCrimes() {
+        List<Crime> result = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Для каждого второго объекта mCrimes.add(crime);
+            result.add(crime);
+        }
+        return result;
+    }
 }
