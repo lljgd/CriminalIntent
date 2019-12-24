4f09848ce97c38179d6fe3c58fbd1c3ce647dde9
Javernaut <javernaut@gmail.com>
2019-12-24 20:03:12 +0200

    Using ViewPager2 on CrimeActivity

--- app/src/main/java/com/example/criminalintent/feature/details/CrimeActivity.java
+++ app/src/main/java/com/example/criminalintent/feature/details/CrimeActivity.java
@@ -4,12 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.criminalintent.R;
+import com.example.criminalintent.data.CrimeStoreProvider;
+import com.example.criminalintent.data.model.Crime;
 
+import java.util.List;
 import java.util.UUID;
 
 public class CrimeActivity extends AppCompatActivity {
@@ -21,15 +29,12 @@ public class CrimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_details);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        ViewPager2 viewPager = findViewById(R.id.pager);
 
-        if (fragmentManager.findFragmentById(R.id.fragment_container) == null) {
-            UUID crimeId = (UUID) getIntent().getSerializableExtra(KEY_ID);
-
-            fragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, CrimeFragment.makeInstance(crimeId))
-                    .commit();
-        }
+        viewPager.setAdapter(new CrimePagesAdapter(
+                CrimeStoreProvider.getInstance(this).getCrimes(),
+                this
+        ));
     }
 
     public static Intent makeIntent(Context context, UUID crimeId) {
